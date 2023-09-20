7555bcdce8c5d0cf1a549d14c940933e7d6bfe05
Adebayo Adesina <adebayoadesina789@gmail.com>
2023-09-20 11:00:49 +0100

    GetLinkedChallenge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
+import MyClassLogo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const App = () => {
+
+  //Javscript
+  const MyFunc = () => {
+
+  }
+
+
+  //HTML
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={MyClassLogo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
