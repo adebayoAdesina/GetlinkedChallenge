f123d9c02e68f51b18c0161ee8bd0ba849c7f93c
Adebayo Adesina <adebayoadesina789@gmail.com>
2023-09-20 18:23:33 +0100

    images and scroll function added.

--- src/utils/image.js
+++ src/utils/image.js
@@ -2,5 +2,18 @@ import BigIdealImage from "../images/image_ideal.png";
 import StarImage from "../images/star.png";
 import ArrowIcon from "../images/arrow.png";
 import RulesImage from "../images/rulesImage.png";
+import JudgingCriteriaImage from "../images/judging.png";
+import ColoredStar from '../images/color_star.png'
+import WhiteStar from '../images/white_star.png'
+import QuestionImage from '../images/question_image.png'
 
-export { BigIdealImage, StarImage, ArrowIcon, RulesImage };
+export {
+  BigIdealImage,
+  StarImage,
+  ArrowIcon,
+  RulesImage,
+  JudgingCriteriaImage,
+  ColoredStar,
+  WhiteStar,
+  QuestionImage,
+};
