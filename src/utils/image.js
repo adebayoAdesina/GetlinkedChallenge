6a38759f734fd30919d2e0041f7937aa57585c68
Adebayo Adesina <adebayoadesina789@gmail.com>
2023-09-20 15:55:42 +0100

    Images folder and images js file added.

---
+++ src/utils/image.js
@@ -0,0 +1,6 @@
+import BigIdealImage from "../images/image_ideal.png";
+import StarImage from "../images/star.png";
+import ArrowIcon from "../images/arrow.png";
+import RulesImage from "../images/rulesImage.png";
+
+export { BigIdealImage, StarImage, ArrowIcon, RulesImage };
